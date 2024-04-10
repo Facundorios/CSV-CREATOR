c5046b6e1957b1e48dcf372063c904b9ba19fb36
Facundo Ríos <Facundorios2005@gmail.com>
2024-04-09 21:45:52 -0300

    feat: files creations

--- Archives/db.py
+++ Archives/db.py
@@ -2,26 +2,26 @@ import sys
 import MySQLdb
 import csv
 
-
 try:
     db = MySQLdb.connect('localhost', 'root','', 'python_db')
 except MySQLdb.Error as e:
     print("Error a conectar a la base de datos:", e)
     sys.exit(1)
-    
+print("Base de datos exitosa")
+
 try:
     cursor = db.cursor()
-    cursor.execute("SELECT DISTINTC provincia FROM localidades")
+    cursor.execute("SELECT DISTINCT provincia from localidades")
     provincias = cursor.fetchall()
     for provincia in provincias:
-        cursor.execute("SELECT * From localidades WHERE provincia = %s", (provincia[0],) )
+        cursor.execute("SELECT * FROM `localidades` WHERE provincia = %s", (provincia[0],) )
         localidades = cursor.fetchall()
-    with open(f'localidades_{provincia}.csv', mode="w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["provincia", "id", "localidad", "cp", "id_prov_mstr"])
-        writer.writerow(localidades)
+        with open(f'Archives/{provincia[0]}.csv', "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(localidades)
             
 except MySQLdb.Error as e:
+        db.rollback()
         print("Error:", e)
 
 
