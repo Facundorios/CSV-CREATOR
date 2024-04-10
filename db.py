64320755fb63b16e1286d0081d2c50b925ea7531
Facundo Rìos <facundorios2005@gmail.com>
2024-04-10 10:45:30 -0300

    muchos cambios

--- db.py
+++ db.py
@@ -9,14 +9,21 @@ except MySQLdb.Error as e:
     sys.exit(1)
 print("Base de datos exitosa")
 
+create_tables = "DROP TABLE localidades; CREATE TABLE IF NOT EXISTS localidades (provincia VARCHAR(100) NOT NULL, id INT NOT NULL, localidad VARCHAR(100) NOT NULL,cp INT(255) NOT NULL,id_prov_mstr INT(255));"
+
+csv_content = "INSERT INTO localidades (provincia, id, localidades, cp, id_prov_mstr) VALUES %s, %s, %s, %s, %s;"
+
+cursor = db.cursor()
+cursor.execute(create_tables) 
+cursor.excecute(csv_content)
+
 try:
-    cursor = db.cursor()
     cursor.execute("SELECT DISTINCT provincia from localidades")
     provincias = cursor.fetchall()
     for provincia in provincias:
-        cursor.execute("SELECT * FROM `localidades` WHERE provincia = %s", (provincia[0],) )
+        cursor.execute("SELECT * FROM `localidades` WHERE provincia = %s", (provincia[0],))
         localidades = cursor.fetchall()
-        with open(f'Archives/{provincia[0]}.csv', "w", newline="") as file:
+        with open(f'Archives/{provincia[0]}.csv', "w", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
             writer.writerow(localidades)
             
